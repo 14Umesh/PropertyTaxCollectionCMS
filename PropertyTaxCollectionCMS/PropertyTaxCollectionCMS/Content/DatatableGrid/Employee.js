991f8e575f5a27e495fda453a3f982fab673dd16
14umesh <umeshl@appynitty.com>
2020-12-24 18:16:16 +0530

    icon added

--- PropertyTaxCollectionCMS/PropertyTaxCollectionCMS/Content/DatatableGrid/Employee.js
+++ PropertyTaxCollectionCMS/PropertyTaxCollectionCMS/Content/DatatableGrid/Employee.js
@@ -23,7 +23,8 @@
                 { "data": "ADUM_EMPLOYEE_ID" },
                 { "data": "ADUM_DESIGNATION" },
                 { "data": "ADUM_MOBILE" },
-                { "render": function (data, type, full, meta) { return '<a  href="javascript:void(0)" onclick="Edit(' + full["ADUM_USER_CODE"] + ')" >Edit</a>'; }, "width": "10%" },
+                //{ "render": function (data, type, full, meta) { return '<a  href="javascript:void(0)" onclick="Edit(' + full["ADUM_USER_CODE"] + ')" >Edit</a>'; }, "width": "10%" },
+                { "render": function (data, type, full, meta) { return '<a  href="javascript:void(0)" class="tooltip1" style="cursor:pointer"   onclick="Edit(' + full["ADUM_USER_CODE"] + ')"  ><i class="material-icons edit-icon">edit</i>'; }, "width": "10%" }
         ]
     });
 
